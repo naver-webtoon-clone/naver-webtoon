c7d3ae3d0c5dc16ceaa3e7ec8c1049ddfd3d6db1
suhjaesuk <suhjaesuk@gmail.com>
2023-05-23 20:46:35 +0900

    feat: 에피소드 서비스 수정, 삭제, 조회 기능 추가

--- src/main/java/com/naver/webtoon/episode/service/EpisodeService.java
+++ src/main/java/com/naver/webtoon/episode/service/EpisodeService.java
@@ -1,9 +1,13 @@
 package com.naver.webtoon.episode.service;
 
 import com.naver.webtoon.common.exception.WebtoonException;
-import com.naver.webtoon.episode.dto.EpisodeRegisterRequest;
+import com.naver.webtoon.common.redis.repository.EpisodeViewRedisRepository;
+import com.naver.webtoon.episode.dto.request.EpisodeRegisterRequest;
+import com.naver.webtoon.episode.dto.request.EpisodeUpdateRequest;
+import com.naver.webtoon.episode.dto.response.EpisodeRetrieveResponse;
 import com.naver.webtoon.episode.entity.Episode;
 import com.naver.webtoon.episode.repository.EpisodeRepository;
+import com.naver.webtoon.member.entity.Member;
 import com.naver.webtoon.webtoon.entity.Webtoon;
 import com.naver.webtoon.webtoon.repository.WebtoonRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +18,12 @@ import java.time.LocalDate;
 
 import static com.naver.webtoon.common.exception.ErrorCode.FREE_EPISODE_MUST_HAVE_FREE_RELEASE_DATE_IS_NULL;
 import static com.naver.webtoon.common.exception.ErrorCode.FREE_RELEASE_DATE_MUST_BE_AFTER_THAN_CURRENT_DATE;
+import static com.naver.webtoon.common.exception.ErrorCode.NOT_FOUND_EPISODE;
 import static com.naver.webtoon.common.exception.ErrorCode.NOT_FOUND_WEBTOON;
+import static com.naver.webtoon.common.exception.ErrorCode.PRIVATE_EPISODE_INACCESSIBILITY;
 import static com.naver.webtoon.common.exception.ErrorCode.PRIVATE_EPISODE_MUST_BE_PAID;
+import static com.naver.webtoon.common.exception.ErrorCode.PUBLIC_EPISODE_INACCESSIBILITY;
 import static com.naver.webtoon.common.exception.ErrorCode.PUBLIC_EPISODE_MUST_BE_FREE;
-
 
 @Service
 @RequiredArgsConstructor
@@ -25,16 +31,17 @@ public class EpisodeService {
 
     private final EpisodeRepository episodeRepository;
     private final WebtoonRepository webtoonRepository;
+    private final EpisodeViewRedisRepository episodeViewRedisRepository;
     private final static int AMOUNT_OF_PUBLIC_EPISODE_COOKIE = 0;
 
     @Transactional
     public void registerEpisode(Long webtoonId, EpisodeRegisterRequest request) {
         boolean isPublic = request.getIsPublic();
         int neededCookieAmount = request.getNeededCookieAmount();
+        LocalDate freeReleaseDate = request.getFreeReleaseDate();
+
         throwIfFreeForPrivateEpisode(isPublic, neededCookieAmount);
         throwIfPaidForPublicEpisode(isPublic, neededCookieAmount);
-
-        LocalDate freeReleaseDate = request.getFreeReleaseDate();
         throwIfFreeReleaseDateEnteredForPublicEpisode(isPublic, freeReleaseDate);
         throwIfFreeReleaseDateIsCurrentDateOrLess(freeReleaseDate);
 
@@ -44,6 +51,71 @@ public class EpisodeService {
         Episode episode = request.toEpisode(webtoon);
 
         episodeRepository.save(episode);
+    }
+
+    @Transactional
+    public void updateEpisode(Long episodeId, EpisodeUpdateRequest request) {
+        String title = request.getTitle();
+        String content = request.getContent();
+        String postscript = request.getPostscript();
+        boolean isPublic = request.getIsPublic();
+        int neededCookieAmount = request.getNeededCookieAmount();
+        LocalDate freeReleaseDate = request.getFreeReleaseDate();
+
+        throwIfFreeForPrivateEpisode(isPublic, neededCookieAmount);
+        throwIfPaidForPublicEpisode(isPublic, neededCookieAmount);
+        throwIfFreeReleaseDateEnteredForPublicEpisode(isPublic, freeReleaseDate);
+        throwIfFreeReleaseDateIsCurrentDateOrLess(freeReleaseDate);
+
+        Episode episode = episodeRepository.findById(episodeId).orElseThrow(
+                () -> new WebtoonException(NOT_FOUND_EPISODE));
+
+        episode.update(title, content, postscript, isPublic, neededCookieAmount, freeReleaseDate);
+    }
+
+    @Transactional
+    public void deleteEpisode(Long episodeId) {
+        Episode episode = episodeRepository.findById(episodeId).orElseThrow(
+                () -> new WebtoonException(NOT_FOUND_EPISODE));
+
+        episodeRepository.delete(episode);
+    }
+
+    @Transactional
+    public EpisodeRetrieveResponse retrievePublicEpisode(Long episodeId) {
+        Episode episode = episodeRepository.findById(episodeId).orElseThrow(
+                () -> new WebtoonException(NOT_FOUND_EPISODE));
+        Webtoon webtoon = episode.getWebtoon();
+
+        boolean isPublic = episode.getIsPublic();
+        throwIfPrivateEpisode(isPublic);
+
+        episodeRepository.increaseViewCountAtomically(episode.getId());
+        episodeViewRedisRepository.increaseWebtoonViewCountWith30DaysAfterExpired(webtoon.getId());
+
+        return EpisodeRetrieveResponse.toResponse(episode);
+    }
+
+    @Transactional
+    public EpisodeRetrieveResponse retrievePrivateEpisode(Member currentMember, Long episodeId) {
+        Episode episode = episodeRepository.findById(episodeId).orElseThrow(
+                () -> new WebtoonException(NOT_FOUND_EPISODE));
+        Webtoon webtoon = episode.getWebtoon();
+
+        boolean isPublic = episode.getIsPublic();
+        throwIfPublicEpisode(isPublic);
+        /*
+        TODO:
+         쿠키 사용 기록 엔티티 생성 후 구현 할 것
+         현재 멤버와 에피소드를 포함하는 쿠키 사용 기록이 테이블에 없다면
+         예외를 반환한다.
+         메서드 명 -> throwIfMemberDontPurchaseEpisode();
+        */
+
+        episodeRepository.increaseViewCountAtomically(episode.getId());
+        episodeViewRedisRepository.increaseWebtoonViewCountWith30DaysAfterExpired(webtoon.getId());
+
+        return EpisodeRetrieveResponse.toResponse(episode);
     }
 
     private void throwIfFreeForPrivateEpisode(boolean isPublic, int neededCookieAmount) {
@@ -72,6 +144,19 @@ public class EpisodeService {
         boolean isFreeReleaseDateEntered =  (freeReleaseDate != null);
         if (isFreeReleaseDateEntered && (freeReleaseDate.isBefore(currentDate) || freeReleaseDate.isEqual(currentDate))) {
             throw new WebtoonException(FREE_RELEASE_DATE_MUST_BE_AFTER_THAN_CURRENT_DATE);
+        }
+    }
+
+    private void throwIfPrivateEpisode(boolean isPublic) {
+        boolean isPrivate = !isPublic;
+        if (isPrivate) {
+            throw new WebtoonException(PRIVATE_EPISODE_INACCESSIBILITY);
+        }
+    }
+
+    private void throwIfPublicEpisode(boolean isPublic) {
+        if (isPublic) {
+            throw new WebtoonException(PUBLIC_EPISODE_INACCESSIBILITY);
         }
     }
 }
