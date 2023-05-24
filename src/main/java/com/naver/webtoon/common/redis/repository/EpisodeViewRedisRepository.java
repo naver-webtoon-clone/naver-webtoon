041481f803f244a323fcbf8a312fb813867c400e
suhjaesuk <suhjaesuk@gmail.com>
2023-05-24 09:49:40 +0900

    feat: 레디스 조회수 저장소 추가

--- src/main/java/com/naver/webtoon/common/redis/repository/EpisodeViewRedisRepository.java
+++ src/main/java/com/naver/webtoon/common/redis/repository/EpisodeViewRedisRepository.java
@@ -4,24 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-
 @Service
 @RequiredArgsConstructor
 public class EpisodeViewRedisRepository {
 
     private final RedisTemplate<String, Long> redisTemplate;
-    private final static int VIEW = 1;
+    private final static long VIEW = 1;
     private final static String KEY_NAME = "webtoonId: ";
 
     public void increaseWebtoonViewCountWith30DaysAfterExpired(Long webtoonId) {
         String key = KEY_NAME + webtoonId;
-        redisTemplate.opsForValue().increment(key, VIEW);
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime after30Days = now.toLocalDate().plusDays(30).atStartOfDay();
-        Duration duration = Duration.between(now, after30Days);
+        long currentTime = System.currentTimeMillis();
 
-        redisTemplate.expire(key, duration);
+        redisTemplate.opsForZSet().incrementScore(key, VIEW, currentTime);
+
+        //TODO: 아래 로직은 스프링 스케쥴러를 이용해 모든 키값에 대하여 자정에 실행되도록 변경
+        long thirtyDaysAgo = currentTime - (30L * 24 * 60 * 60 * 1000);
+        
+        redisTemplate.opsForZSet().removeRangeByScore(key, 0, thirtyDaysAgo);
     }
 }
