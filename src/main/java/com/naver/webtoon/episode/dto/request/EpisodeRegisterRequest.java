c5c80f67e1d8fc90b6a7fce3e407d5e8d4fd661d
JaeSuk <110963294+suhjaesuk@users.noreply.github.com>
2023-06-08 15:49:47 +0900

    Redis 관련 설정, Episode 변경, 삭제, 조회 기능 추가  (#7)
    * form : 주석 제거
    
    * feat : validation 그룹 추가
    
    * feat : episode 도메인 추가
    
    * feat : episode 등록 기능 작성
    
    * feat : 메서드명 변경 throwIfPaidForFreeEpisode -> throwIfPaidForPublicEpisode
    
    * feat: redis 의존 추가
    
    * feat: 에피소드 관련 에러코드 추가
    
    * feat: 컨트롤러 에피소드 수정, 삭제, 조회 기능 추가
    
    * feat: 에피소드 엔티티 update 메서드 작성
    
    * feat: 에피소드 조회 시 원자적 쿼리로 업데이트되는 레포지토리 메서드 추가
    
    * feat: 에피소드 서비스 수정, 삭제, 조회 기능 추가
    
    * feat: Redis 설정 추가
    
    * feat: 조회 시 웹툰별로 30일간의 조회수가 저장되는 RedisRepository 추가
    
    * feat: 에피소드 업데이트 시 필요한 request dto 추가
    
    * feat: 에피소드 조회 시 필요한 response dto 추가
    
    * feat: redis 환경변수 파일
    
    * feat: 레디스 조회수 저장소 추가

---
+++ src/main/java/com/naver/webtoon/episode/dto/request/EpisodeRegisterRequest.java
@@ -0,0 +1,37 @@
+package com.naver.webtoon.episode.dto.request;
+
+import com.naver.webtoon.episode.entity.Episode;
+import com.naver.webtoon.webtoon.entity.Webtoon;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EpisodeRegisterRequest {
+
+    private String title;
+    private String content;
+    private String postscript;
+    private Boolean isPublic;
+    @Min(value = 0, message =  "필요한 쿠키 양은 0개 이상입니다.")
+    private Integer neededCookieAmount;
+    private LocalDate freeReleaseDate;
+
+    public Episode toEpisode(Webtoon webtoon){
+        return Episode.builder()
+                .title(title)
+                .content(content)
+                .postscript(postscript)
+                .views(0)
+                .isPublic(isPublic)
+                .neededCookieAmount(neededCookieAmount)
+                .freeReleaseDate(freeReleaseDate)
+                .webtoon(webtoon)
+                .build();
+    }
+}
